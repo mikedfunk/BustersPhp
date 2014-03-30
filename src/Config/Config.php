67cb46fdf68ff9822ad06f9c20392a6574a7704a
Mike Funk <mike@mikefunk.com>
2014-03-29 18:44:46 -0700

    added dot between base paths and hashes

--- src/Config/Config.php
+++ src/Config/Config.php
@@ -11,7 +11,7 @@ return array(
     /**
      * the template used to create the css link tag
      */
-    'cssTemplate' => '<link href="{{CSS_BASE_PATH}}{{HASH}}.css" rel="stylesheet">',
+    'cssTemplate' => '<link href="{{CSS_BASE_PATH}}.{{HASH}}.css" rel="stylesheet">',
 
     /**
      * the path to the js cache folder
@@ -21,5 +21,5 @@ return array(
     /**
      * the template used to create the js script tag
      */
-    'jsTemplate' => '<script src="{{JS_BASE_PATH}}{{HASH}}.js"></script>',
+    'jsTemplate' => '<script src="{{JS_BASE_PATH}}.{{HASH}}.js"></script>',
 );
