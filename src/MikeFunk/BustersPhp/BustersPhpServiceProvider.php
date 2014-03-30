99162665fdca419eb47db49850dc186e9cfa0d14
Mike Funk <mike@mikefunk.com>
2014-03-29 18:32:11 -0700

    added laravel service provider

---
+++ src/MikeFunk/BustersPhp/BustersPhpServiceProvider.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Laravel Service Provider for BustersPhp
+ *
+ * @package BustersPhp
+ * @license MIT License <http://opensource.org/licenses/mit-license.html>
+ */
+namespace MikeFunk\BustersPhp;
+
+use Illuminate\Support\ServiceProvider;
+
+/**
+ * BustersPhpServiceProvider
+ *
+ * @author Michael Funk <mike@mikefunk.com>
+ */
+class BustersPhpServiceProvider
+{
+
+    /**
+     * Bind the interface to the implementation
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->bind(
+            'MikeFunk\BustersPhp\BustersPhpInterface',
+            'MikeFunk\BustersPhp\BustersPhp'
+        );
+    }
+}
